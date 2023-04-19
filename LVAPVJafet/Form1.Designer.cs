90853ac856a161faadaa5884a1e71f88af5ce9a7
uzielcamp <uziel.camp776@gmail.com>
2023-04-18 20:24:54 -0600

    se movieron los botones

--- LVAPVJafet/Form1.Designer.cs
+++ LVAPVJafet/Form1.Designer.cs
@@ -45,9 +45,10 @@ namespace LVAPVJafet
             // 
             // bttAbrirfrm2
             // 
-            this.bttAbrirfrm2.Location = new System.Drawing.Point(572, 361);
+            this.bttAbrirfrm2.Location = new System.Drawing.Point(750, 394);
+            this.bttAbrirfrm2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bttAbrirfrm2.Name = "bttAbrirfrm2";
-            this.bttAbrirfrm2.Size = new System.Drawing.Size(174, 58);
+            this.bttAbrirfrm2.Size = new System.Drawing.Size(232, 71);
             this.bttAbrirfrm2.TabIndex = 0;
             this.bttAbrirfrm2.Text = "Registrarse";
             this.bttAbrirfrm2.UseVisualStyleBackColor = true;
@@ -55,9 +56,10 @@ namespace LVAPVJafet
             // 
             // bttCerrar
             // 
-            this.bttCerrar.Location = new System.Drawing.Point(44, 361);
+            this.bttCerrar.Location = new System.Drawing.Point(207, 389);
+            this.bttCerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.bttCerrar.Name = "bttCerrar";
-            this.bttCerrar.Size = new System.Drawing.Size(147, 64);
+            this.bttCerrar.Size = new System.Drawing.Size(196, 79);
             this.bttCerrar.TabIndex = 1;
             this.bttCerrar.Text = "Acceder";
             this.bttCerrar.UseVisualStyleBackColor = true;
@@ -67,43 +69,48 @@ namespace LVAPVJafet
             // 
             this.lblusername.AutoSize = true;
             this.lblusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblusername.Location = new System.Drawing.Point(179, 200);
+            this.lblusername.Location = new System.Drawing.Point(239, 246);
+            this.lblusername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblusername.Name = "lblusername";
-            this.lblusername.Size = new System.Drawing.Size(110, 25);
+            this.lblusername.Size = new System.Drawing.Size(139, 31);
             this.lblusername.TabIndex = 2;
             this.lblusername.Text = "Username";
             this.lblusername.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(326, 200);
+            this.textBox1.Location = new System.Drawing.Point(435, 246);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.Size = new System.Drawing.Size(273, 22);
             this.textBox1.TabIndex = 3;
             // 
             // lblpassword
             // 
             this.lblpassword.AutoSize = true;
             this.lblpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblpassword.Location = new System.Drawing.Point(179, 272);
+            this.lblpassword.Location = new System.Drawing.Point(239, 335);
+            this.lblpassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblpassword.Name = "lblpassword";
-            this.lblpassword.Size = new System.Drawing.Size(106, 25);
+            this.lblpassword.Size = new System.Drawing.Size(134, 31);
             this.lblpassword.TabIndex = 4;
             this.lblpassword.Text = "Password";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(326, 272);
+            this.textBox2.Location = new System.Drawing.Point(435, 335);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(206, 20);
+            this.textBox2.Size = new System.Drawing.Size(273, 22);
             this.textBox2.TabIndex = 5;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::LVAPVJafet.Properties.Resources.gato_romano_0;
-            this.pictureBox3.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox3.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(214, 137);
+            this.pictureBox3.Size = new System.Drawing.Size(285, 169);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 8;
             this.pictureBox3.TabStop = false;
@@ -111,9 +118,10 @@ namespace LVAPVJafet
             // pictureBox2
             // 
             this.pictureBox2.Image = global::LVAPVJafet.Properties.Resources.por_que_nos_parece_que_los_perros_sonrien_una_historia_de_30_000_anos;
-            this.pictureBox2.Location = new System.Drawing.Point(547, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(729, 15);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(214, 137);
+            this.pictureBox2.Size = new System.Drawing.Size(285, 169);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
@@ -121,19 +129,20 @@ namespace LVAPVJafet
             // pictureBox1
             // 
             this.pictureBox1.Image = global::LVAPVJafet.Properties.Resources.razas_de_conejos_enanos_o_toy_24086_600;
-            this.pictureBox1.Location = new System.Drawing.Point(288, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(384, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(214, 137);
+            this.pictureBox1.Size = new System.Drawing.Size(285, 169);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // frmbusquedadeanimales
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.NavajoWhite;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -143,6 +152,7 @@ namespace LVAPVJafet
             this.Controls.Add(this.lblusername);
             this.Controls.Add(this.bttCerrar);
             this.Controls.Add(this.bttAbrirfrm2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmbusquedadeanimales";
             this.Text = "Acceso";
             this.Load += new System.EventHandler(this.frmbusquedadeanimales_Load);
